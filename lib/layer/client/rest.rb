ebed64bdf08efc9b9387e66f9d97decb724f89d6
Benedikt Deicke <benedikt@synatic.net>
2015-10-06 13:35:09 +0200

    Adds specs for the REST client

--- lib/layer/client/rest.rb
+++ lib/layer/client/rest.rb
@@ -2,23 +2,39 @@ module Layer
   class Client
     class REST < Layer::Client
 
-      attr_reader :token
-
-      def initialize(app_id = self.class.app_id)
+      def self.authenticate(app_id, nonce = nil)
         client = Layer::Client.new
-        nonce = client.post('/nonces')['nonce']
+
+        nonce ||= client.post('/nonces')['nonce']
+
         identity_token = yield(nonce)
 
-        @token = client.post('/sessions', { identity_token: identity_token, app_id: app_id })['session_token']
+        response = client.post('/sessions', { identity_token: identity_token, app_id: app_id })
+        response['session_token']
+      end
+
+      attr_reader :token, :app_id, :block
+
+      def initialize(app_id = self.class.app_id, &block)
+        @app_id = self.class.normalize_id(app_id)
+        @block = block
+        authenticate
       end
 
     private
 
       def request(method, url, payload = {}, headers = {})
-        url = "https://api.layer.com/#{url}" unless url.start_with?('https://api.layer.com')
+        url = "https://api.layer.com#{url}" unless url.start_with?('https://api.layer.com')
         headers['Authorization'] ||= "Layer session-token=\"#{token}\""
 
         super
+      rescue Layer::Exceptions::AuthenticationRequired => exception
+        authenticate(exception.response_json['data']['nonce'])
+        retry
+      end
+
+      def authenticate(nonce = nil)
+        @token = self.class.authenticate(app_id, nonce, &block)
       end
     end
   end
