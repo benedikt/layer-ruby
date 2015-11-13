fe597ef588aae51a99e5f54b0d0f2254bd18db86
Benedikt Deicke <benedikt@synatic.net>
2015-11-13 15:33:37 +0100

    Adds documentation for Webhooks

--- lib/layer/webhook.rb
+++ lib/layer/webhook.rb
@@ -1,4 +1,15 @@
 module Layer
+  # @example
+  #   webhook = Layer::Webhook.create({
+  #     target_config: { foo: :bar },
+  #     secret: 'my-secret',
+  #     event_types: ['message.sent'],
+  #     target_url: 'http://example.com/layer/webhook'
+  #   })
+  # @see https://gist.github.com/vtrehan/9ef1e16db5e74305ddf3 Layer Webhook documentation (Prerelease)
+  # @note This feature is still in beta at Layer. Please contact Layer to enable webhooks for your account.
+  #
+  # @!macro platform-api
   class Webhook < Resource
     include Operations::Find
     include Operations::List
@@ -10,26 +21,36 @@ module Layer
     # @!parse extend Layer::Operations::Create::ClassMethods
     # @!parse extend Layer::Operations::Delete::ClassMethods
 
+    # Activate this webhook
     def activate!
       client.post("#{url}/activate")
     end
 
+    # Deactivate this webhook
     def deactivate!
       client.post("#{url}/deactivate")
     end
 
+    # Check if this webhook is unverified
+    # @return [Boolean] whether the webhook is unverified
     def unverified?
       status == 'unverified'
     end
 
+    # Check if this webhook is active
+    # @return [Boolean] whether the webhook is active
     def active?
       status == 'active'
     end
 
+    # Check if this webhook is inactive
+    # @return [Boolean] whether the webhook is inactive
     def inactive?
       status == 'inactive'
     end
 
+    # Returns the time the webhook was created at
+    # @return [Time] the time the webhook was created at
     def created_at
       Time.parse(attributes['created_at'])
     end
